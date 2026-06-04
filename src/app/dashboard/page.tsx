1ab53f0d286b2f5451494d49a95bf874926df977
houssamelbadaoui <houssamelbadaoui5@email.com>
2026-06-04 19:44:35 +0200

    Add Dashboard structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,38 +1,26 @@
-import {
-  Sidebar,
-  SidebarProvider,
-  SidebarContent,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-} from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function Dashboard() {
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen">
-        <Sidebar>
-          <SidebarHeader>Task Manager</SidebarHeader>
-          <SidebarContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton>All Tasks</SidebarMenuButton>
-              </SidebarMenuItem>
-
-              <SidebarMenuItem>
-                <SidebarMenuButton>Completed</SidebarMenuButton>
-              </SidebarMenuItem>
-
-              <SidebarMenuItem>
-                <SidebarMenuButton>Pending</SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarContent>
-        </Sidebar>
-
-        <main className="flex-1 p-6">Dashboard</main>
-      </div>
-    </SidebarProvider>
+    <div className="min-h-screen flex bg-gray-50">
+      <aside className="w-64 h-full px-3 py-4 bg-neutral-primary-soft border-e border-default">
+        <h2 className="text-2xl font-bold mb-4 text-center">Task Manager</h2>
+        <ul className="space-y-2 font-medium">
+          <li className="text-center mb-4">All tasks</li>
+          <li className="text-center mb-4">Completed</li>
+          <li className="text-center mb-4">Pending</li>
+        </ul>
+      </aside>
+      <main className="flex-1 flex flex-col">
+        <header className="flex items-center justify-between w-full px-6 py-4 border-b bg-white">
+          <div>
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <p className="text-sm text-gray-500">Manage your daily tasks</p>
+          </div>
+          <Button>+ Add Task</Button>
+        </header>
+        <section className="p-6"></section>
+      </main>
+    </div>
   );
 }
