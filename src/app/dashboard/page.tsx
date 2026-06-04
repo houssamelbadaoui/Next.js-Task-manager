9042a08e84c0e22abf67c8b9f98ae916623571da
houssamelbadaoui <houssamelbadaoui5@email.com>
2026-06-04 20:12:22 +0200

    Add Dialog component to recieve input

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,19 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Field, FieldGroup } from "@/components/ui/field";
 
 export default function Dashboard() {
   return (
@@ -17,7 +32,44 @@ export default function Dashboard() {
             <h1 className="text-2xl font-bold">Dashboard</h1>
             <p className="text-sm text-gray-500">Manage your daily tasks</p>
           </div>
-          <Button>+ Add Task</Button>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>+ Add Task</Button>
+            </DialogTrigger>
+
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add Task</DialogTitle>
+                <DialogDescription>Create a new Task here</DialogDescription>
+              </DialogHeader>
+
+              <div className="space-y-6 py-4">
+                <FieldGroup>
+                  <label htmlFor="title">Title</label>
+                  <Input id="title" placeholder="Enter task title" />
+                </FieldGroup>
+
+                <FieldGroup>
+                  <label htmlFor="description">Description</label>
+                  <Input
+                    id="description"
+                    placeholder="Enter task description"
+                  />
+                </FieldGroup>
+              </div>
+
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+
+                <Button className="bg-indigo-600 hover:bg-indigo-700">
+                  Add Task
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </header>
         <section className="p-6"></section>
       </main>
